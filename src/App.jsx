3df00142749890b662ff73444614cd6765e8ba6b
João Marcelo Dantas <jmgrd98@gmail.com>
2023-01-09 12:34:34 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,62 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
-import Header from './components/Header/Header'
 
 function App() {
 
-  const [cards, setCards] = useState([
-    {
-      id: 1,
-      cep: `dasdsa`,
-      endereco: 'adsad'
-    }
-  ])
+const [ceps, setCeps] = useState([])
+const [inputText, setInputText] = useState('')
+
+  function handleChange(e){
+    setInputText(e.target.value)
+}
+
+function handleSubmit(e){
+    e.preventDefault()
+    fetchAPI(url)
+    setCeps([...ceps, inputText])
+    setInputText('')
+    console.log(...ceps)
+}
+
+async function fetchAPI(e){ 
+  e.preventDefault() 
+  await fetch(`https://viacep.com.br/ws/${inputText}/json/`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch((error) => console.log(error))
+
+    setCeps([...ceps, inputText])
+    setInputText('')
+}
 
   return (
     <div className="App">
-      <Header></Header>
+
+      <header>
+
+        <h1>BuscaCEP</h1>
+
+        <form onSubmit={fetchAPI} className="SearchForm">
+            <input
+            onChange={handleChange}
+            value={inputText}
+            type="text"
+            placeholder="Pesquise um CEP"
+            />
+            <button type="submit">Buscar</button>
+        </form>
+        </header>
 
       <main>
         <p>Insira um CEP!</p>
 
-        {cards.map(card => {
+        {ceps.map(cep => {
           return(
             <Card
-            key={card.id}
-            cep={card.cep}
-            endereco={card.endereco}/>
+            key={cep} 
+            cep={cep}
+            endereco={cep}/>
           )
         })}
       </main>
