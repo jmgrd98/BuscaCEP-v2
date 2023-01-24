f791aa96b7ac94e3626c7f22fd91bbf334b79a95
João Marcelo Dantas <jmgrd98@gmail.com>
2023-01-24 12:23:28 -0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,20 @@
 import { CardStyle } from "./style";
 import {FaMapMarkedAlt} from 'react-icons/fa'
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 
 export default function Card({cep, uf, cidade, bairro, logradouro}){
 
-  navigator.geolocation.getCurrentPosition(success)
+const [position, setPosition] = useState([51.505, -0.09])
+
+navigator.geolocation.getCurrentPosition(success)
 
 function success(position){
 
   const lat = position.coords.latitude
   const lon = position.coords.longitude
 
+    setPosition([lat, lon])
     console.log(lat, lon)
 }
 
@@ -33,12 +37,12 @@ function success(position){
                 <button onClick={success}><FaMapMarkedAlt size={25}/></button>
                 </div>
 
-<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className='mapContainer'>
+<MapContainer center={position} zoom={13} scrollWheelZoom={false} className='mapContainer'>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[51.505, -0.09]}>
+  <Marker position={position}>
     <Popup>
       Você está aqui!
     </Popup>
