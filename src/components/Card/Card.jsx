cfa7f95600f01f8666497a365c6053676df1132d
João Marcelo Dantas <jmgrd98@gmail.com>
2023-01-24 16:44:12 -0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,22 +1,27 @@
 import { CardStyle } from "./style";
 import {FaMapMarkedAlt} from 'react-icons/fa'
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Card({cep, uf, cidade, bairro, logradouro}){
 
 const [position, setPosition] = useState([51.505, -0.09])
+const [zoom, setZoom] = useState(13)
+const [map, setMap] = useState(null)
 
-navigator.geolocation.getCurrentPosition(success)
-
-function success(position){
-
-  const lat = position.coords.latitude
-  const lon = position.coords.longitude
-
+useEffect(() => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude
+    const lon = position.coords.longitude
     setPosition([lat, lon])
-    console.log(lat, lon)
-}
+  })
+}, []) // roda no 1o mount
+
+useEffect(() => {
+  if (map) {
+    map.setView(position, zoom)
+  }
+}, [map, position, zoom])
 
 
 
@@ -34,10 +39,10 @@ function success(position){
                 <p>{bairro}</p>
                 <p>{logradouro}</p>
 
-                <button onClick={success}><FaMapMarkedAlt size={25}/></button>
+                <button onClick={setPosition([lat, lon])}><FaMapMarkedAlt size={25}/></button>
                 </div>
 
-<MapContainer center={position} zoom={13} scrollWheelZoom={false} className='mapContainer'>
+<MapContainer center={position} zoom={zoom} scrollWheelZoom={false} ref={setMap} className='mapContainer'>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
